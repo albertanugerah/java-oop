87e54d3ee2e367c1c0e3cbed3e05be0367e72d28
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-03-03 20:44:29 +0700

    membuat field didalam class Person dan memanipulasinya di PersonApp

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,6 @@
 class Person {
-
+    String name;
+    String address;
+    final String country = "Indonesia";
+//    final adalah kata kunci untuk menandakan tidak bisa diubah data/valuenya, dan langsung di deklarasikan"
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,13 +2,13 @@ public class PersonApp {
     public static void main(String[] args) {
         var person1 = new Person();
 
+        person1.name = "Albert";
+        person1.address = "Kreo";
+//        person1.country = "Singapore";//ERROR
+
         Person person2 = new Person();
 
         Person person3;
         person3 = new Person();
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
     }
 }
