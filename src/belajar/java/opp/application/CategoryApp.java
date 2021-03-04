61a7183ee6773e16caf97c3ba8a56327bb39523f
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-03-05 01:36:42 +0700

    Getter and Setter berawal dari encapsulation, encapsulation adalah memastikan data sensitif sebuah object tersembunyi dari akses luark agar kita bisa menjaga data sebuah object tetatp baik dan valid.di java, proses encapsulation dibuat dengan getter dan setter method, getter untuk mengambil data field dan setter untuk mengubah data field

---
+++ src/belajar/java/opp/application/CategoryApp.java
@@ -0,0 +1,17 @@
+package belajar.java.opp.application;
+
+import belajar.java.opp.data.Category;
+
+public class CategoryApp {
+    public static void main(String[] args) {
+
+        var category = new Category();
+        category.setId("ID");
+        category.setId(null);
+
+
+        System.out.println(category.getId());
+
+
+    }
+}
