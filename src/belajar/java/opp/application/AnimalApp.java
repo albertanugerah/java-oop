f11626bc000d02e2fc4454ab706734eb2a5bbd49
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-03-05 00:41:58 +0700

    abstract method, membuat abstract method, tidak boleh membuat block method untk method tersebut, jadi abstrack method wajid di override di class childnya dan tidak boleh memiliki access modifier private

---
+++ src/belajar/java/opp/application/AnimalApp.java
@@ -0,0 +1,14 @@
+package belajar.java.opp.application;
+
+import belajar.java.opp.data.Animal;
+import belajar.java.opp.data.Cat;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+
+        Animal animal = new Cat();
+        animal.name = "Meong";
+        animal.run();
+
+    }
+}
---
+++ src/belajar/java/opp/data/Cat.java
@@ -0,0 +1,9 @@
+package belajar.java.opp.data;
+
+public class Cat extends Animal{
+
+    @Override
+    public void run() {
+        System.out.println("Cat " + name + " is Running");
+    }
+}
