e7fd9ad9edd8a5e69b2976417bfbf3345fa19a9d
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-03-05 21:17:52 +0700

    implementasi try with resource

--- src/belajar/java/opp/application/ReadApp.java
+++ src/belajar/java/opp/application/ReadApp.java
@@ -7,11 +7,8 @@ import java.io.IOException;
 public class ReadApp {
     public static void main(String[] args) {
 
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(
-                    new FileReader("README.md")
-            );
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("README.md"))){
 
             while (true){
                 String line = reader.readLine();
@@ -24,16 +21,6 @@ public class ReadApp {
 
             System.out.println("Error membaca file" + throwable.getMessage());
 
-        }finally {
-            if(reader != null){
-                try {
-                    reader.close();
-                    System.out.println("Sukses menutup");
-                }catch (IOException exception){
-                    System.out.println("Error menutup resource" + exception.getMessage());
-                }
-
-            }
         }
     }
 }
